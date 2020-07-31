7b45d66e79e210d9f3d8964923f3655ead49271d
Jules Garrett <jrgarrett3222@gmail.com>
2020-07-31 01:34:46 -0500

    change chart

--- page2.js
+++ page2.js
@@ -3,21 +3,9 @@ async function graph2(){
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
-  // parse the date / time
-  var parseTime = d3.timeParse("%Y%m%d");
-
   // set the ranges
-  var x = d3.scaleTime().range([0, width]);
+  var x = d3.scaleBand().range([0, width]).padding(0.4);
   var y = d3.scaleLinear().range([height, 0]);
-
-  // define the 1st line
-  var valueline = d3.line()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.positive); });
-
-  var valueline2 = d3.line()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.death); });
 
 
   // append the svg obgect to the body of the page
@@ -32,33 +20,20 @@ async function graph2(){
             "translate(" + margin.left + "," + margin.top + ")");
 
   // Get the data
-  d3.csv("RaceDataEntry-CRDT.csv", function(error, data) {
+  d3.csv("state_data_20200726.csv", function(error, data) {
     if (error) throw error;
 
     // format the data
     data.forEach(function(d) {
-        d.date = parseTime(d.Date);
-        d.positive = +d.Cases_Total;
-        d.death = +d.Cases_White;
+        d.state = d.State;
+        d.death = +d.Deaths_Total;
     });
 
-
     // Scale the range of the data
-    x.domain(d3.extent(data, function(d) { return d.date; }));
+    x.domain(data.map(function(d) { return d.state; }));
     y.domain([0, d3.max(data, function(d) {
-  	  return Math.max(d.positive); })]);
+      return Math.max(d.positive); })]);
 
-    // Add the valueline path.
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .attr("d", valueline);
-
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .style("stroke", "red")
-        .attr("d", valueline2);
 
     // Add the X Axis
     svg.append("g")
@@ -68,6 +43,16 @@ async function graph2(){
     // Add the Y Axis
     svg.append("g")
         .call(d3.axisLeft(y));
+
+    svg.selectAll("bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.state); })
+        .attr("y", function(d) { return y(d.death); })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height - y(d.death); });
 
   });
 }
