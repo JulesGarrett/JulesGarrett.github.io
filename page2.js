36b37488698292a106ea9831237a3be2a249301b
Jules Garrett <jrgarrett3222@gmail.com>
2020-07-30 22:57:28 -0500

    make seperate files

--- page2.js
+++ page2.js
@@ -1,4 +1,4 @@
-async function graph1(){
+async function graph2(){
   var margin = {top: 20, right: 20, bottom: 30, left: 70},
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
@@ -23,7 +23,7 @@ async function graph1(){
   // append the svg obgect to the body of the page
   // appends a 'group' element to 'svg'
   // moves the 'group' element to the top left margin
-  var svg = d3.select("#dataviz_area").append("svg")
+  var svg = d3.select("#dataviz_area2").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .style("background-color", 'white')
@@ -38,8 +38,8 @@ async function graph1(){
     // format the data
     data.forEach(function(d) {
         d.date = parseTime(d.date);
-        d.positive = +d.positive;
-        d.death = +d.death;
+        d.positive = +d.Cases_Total;
+        d.death = +d.Cases_White;
     });
 
 
