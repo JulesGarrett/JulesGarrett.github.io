87a3aed5256408c23d5c711daa6121da7f434072
Jules Garrett <jrgarrett3222@gmail.com>
2020-08-02 04:36:13 -0500

    logging

--- page3.js
+++ page3.js
@@ -82,9 +82,9 @@ d3.csv("perc_demog_20200726_clean.csv").get(function(data) {
         .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
         .enter().append("rect")
           .attr("x", function(d) { return xSubgroup(d.key); })
-          .attr("y", function(d) { return y(d.value); })
+          .attr("y", function(d) {console.log(y(d.value)); return y(d.value); })
           .attr("width", xSubgroup.bandwidth())
-          .attr("height", function(d) { return height - y(d.value); })
+          .attr("height", function(d) {console.log(height - y(d.value)); return height - y(d.value); })
           .attr("fill", function(d) { return color(d.key); });
   }
   update('United States')
