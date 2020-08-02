19a4d61244b36cc565801c74324bd18281b2cf75
Jules Garrett <jrgarrett3222@gmail.com>
2020-08-02 03:22:30 -0500

    rearrange

--- page3.js
+++ page3.js
@@ -14,66 +14,60 @@ var svg = d3.select("#dataviz_area").append("svg")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+// Get the data
+d3.csv("perc_demog_20200726_clean.csv", function(error, data) {
+  if (error) throw error;
 
-function update(selectedVar){
+  var all_states = d3.map(data, function(d){return(d.state)}).keys()
+  var subgroups = data.columns.slice(1)
+  var groups = d3.map(data, function(d){return(d.group)}).keys()
 
-  // Get the data
-  d3.csv("perc_demog_20200726_clean.csv", function(error, data) {
-    if (error) throw error;
-
-    var all_states = d3.map(data, function(d){return(d.state)}).keys()
-    var subgroups = data.columns.slice(1)
-    var groups = d3.map(data, function(d){return(d.group)}).keys()
-
-    var dropdownButton = d3.select("#selectButton")
-    dropdownButton
-      .selectAll('myOptions')
-      .data(all_states)
-      .enter()
-      .append('option')
-      .text(function (d) { return d; })
-      .attr("value", function (d) { return d; })
-
-      // dropdownButton.on("change", function(d) {
-      //   var selectedOption = d3.select(this).property("value")
-      //   updateChart(selectedOption)
-      // })
+  var dropdownButton = d3.select("#selectButton")
+  dropdownButton
+    .selectAll('myOptions')
+    .data(all_states)
+    .enter()
+    .append('option')
+    .text(function (d) { return d; })
+    .attr("value", function (d) { return d; })
 
 
-    var x = d3.scaleBand()
-        .range([0, width])
-        .padding(0.2)
-        .domain(groups);
+  var x = d3.scaleBand()
+      .range([0, width])
+      .padding(0.2)
+      .domain(groups);
 
-    var xAxis = svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x).tickSize(0));
-
-
-    var y = d3.scaleLinear()
-        .range([height, 0])
-        .domain([0,1]);
-
-    var yAxis = svg.append("g")
-        .call(d3.axisLeft(y));
+  var xAxis = svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x).tickSize(0));
 
 
-    // X axis
-    x.domain(data.map(function(d) { return d.group; }))
-    xAxis.transition().duration(1000).call(d3.axisBottom(x))
+  var y = d3.scaleLinear()
+      .range([height, 0])
+      .domain([0,1]);
 
-    // Add Y axis
-    y.domain([0, 1]);
-    yAxis.transition().duration(1000).call(d3.axisLeft(y));
+  var yAxis = svg.append("g")
+      .call(d3.axisLeft(y));
 
-    var xSubgroup = d3.scaleBand()
-        .domain(subgroups)
-        .range([0, x.bandwidth()])
-        .padding([0.05])
 
-    var color = d3.scaleOrdinal()
-        .domain(subgroups)
-        .range(['#e41a1c','#377eb8','#4daf4a'])
+  // X axis
+  x.domain(data.map(function(d) { return d.group; }))
+  xAxis.transition().duration(1000).call(d3.axisBottom(x))
+
+  // Add Y axis
+  y.domain([0, 1]);
+  yAxis.transition().duration(1000).call(d3.axisLeft(y));
+
+  var xSubgroup = d3.scaleBand()
+      .domain(subgroups)
+      .range([0, x.bandwidth()])
+      .padding([0.05])
+
+  var color = d3.scaleOrdinal()
+      .domain(subgroups)
+      .range(['#e41a1c','#377eb8','#4daf4a'])
+
+  function update(selectedVar){
 
     data = data.filter(function(d){return d.state==selectedVar})
 
@@ -102,7 +96,6 @@ function update(selectedVar){
     })
 
 }
-)}
-
 // Initialize plot
 update('United States')
+)}
